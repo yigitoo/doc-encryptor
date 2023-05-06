313daddc90d41cda594c002d6e5ccaf7282ccb3a
Yiğit GÜMÜŞ <rawns0909@gmail.com>
2023-05-06 23:38:01 +0300

    .

--- db.py
+++ db.py
@@ -1,12 +1,10 @@
 import pymongo
-import certifi
 import dotenv
 import os
 config = dotenv.load_dotenv()
 
 def create_client(cluster_name: str = "user"): 
     db_link = os.environ['DB_URL']
-    cacert = certifi.where()
     client = pymongo.MongoClient(db_link, tlsCAFile=cacert, tlsAllowInvalidCertificates=False)
     databases = client[os.environ["DB_NAME"]]
     return databases[cluster_name]
