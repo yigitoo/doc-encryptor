732ed2b07a0aaa497dc4811225b81d3b4f8b59be
Yiğit GÜMÜŞ <rawns0909@gmail.com>
2023-05-07 11:42:46 +0300

    .

--- view.py
+++ view.py
@@ -1,33 +1,56 @@
 import webview
-import keyboard
 import threading
 from wsgi import app
-import sys
 import os
 import platform
+import pynput
 
-if platform.system().lower() in ['linux', 'darwin']:
-    if not os.geteuid() == 0:
-        print("BU DOSYAYI LÜTFEN ROOT OLARAK ÇALIŞTIRIN!")
-        raise SystemExit(1)
-elif (platform.system() == "Windows"):
-    import pyuac
-    if not pyuac.isUserAdmin():
-        pyuac.runAsAdmin()
-    else:
-        pass
-else:
+if platform.system().lower() not in ['linux', 'darwin', 'windows']:
     print("PLATFORM DESTEKLENMİYOR!\nPlatform: " + platform.system())
 	
 
+class KeyPressed(Exception): pass
 
+"""
+@brief: Deprecated code
 def detect_keys():
     keys = ['alt', 'alt gr', 'ctrl', 'left alt', 'left ctrl', 'left shift', 'right alt', 'right ctrl', 'right shift', 'shift', 'windows']
     while True:
         if keyboard.read_key() in keys:
             window.destroy()
-            raise SystemExit
+            raise SystemExit(1)
+"""
 
+def detect_keys():
+    '@brief: Key Listener'
+    def on_press(key):
+        if key in [
+        pynput.keyboard.Key.print_screen,
+        pynput.keyboard.Key.alt,
+        pynput.keyboard.Key.alt_l,
+        pynput.keyboard.Key.alt_r,
+        pynput.keyboard.Key.alt_gr,
+        pynput.keyboard.Key.cmd,
+        pynput.keyboard.Key.cmd_l,
+        pynput.keyboard.Key.cmd_r,
+        pynput.keyboard.Key.ctrl,
+        pynput.keyboard.Key.ctrl_l,
+        pynput.keyboard.Key.ctrl_r,
+        pynput.keyboard.Key.shift,
+        pynput.keyboard.Key.shift_l,
+        pynput.keyboard.Key.shift_r,
+    ]:
+            raise KeyPressed(key)
+    
+    with pynput.keyboard.Listener(on_press=on_press) as listener:
+        try:
+            listener.join()
+        except KeyPressed as event:
+            window.destroy()
+            os.system('cls || clear')
+            print(f'Şüpheli bir tuşa basıldığı için program açılamıyor.')
+            raise SystemExit(1)
+        
 keyboard_event = threading.Thread(target=detect_keys, daemon=True)
 keyboard_event.start()
 
